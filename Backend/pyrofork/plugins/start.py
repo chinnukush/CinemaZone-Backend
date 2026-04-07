75aab7d75d4602b513f21ba09616612985f8c88e
chinnukush <chinnukushal123@gmail.com>
2026-04-07 09:18:20 +0530

    Update start.py

--- Backend/pyrofork/plugins/start.py
+++ Backend/pyrofork/plugins/start.py
@@ -55,7 +55,7 @@ async def start(bot: Client, message: Message):
 
     # --- Case 1: Plain /start ---
     if not command_part.startswith("file_"):
-        await message.reply_text("ʜɪɪ 👋 ɪ ᴀᴍ ʜᴇʀᴇ ᴛᴏ ᴘʀᴏᴠɪᴅᴇ ᴅɪʀᴇᴄᴛ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋꜱ ғᴏʀ ᴍᴏᴠɪᴇꜱ & ꜱᴇʀɪᴇꜱ ғʀᴏᴍ https://cinemazone-g5rb.vercel.app 📥.")
+        await message.reply_text("ʜɪɪ 👋 ɪ ᴀᴍ ʜᴇʀᴇ ᴛᴏ ᴘʀᴏᴠɪᴅᴇ ᴅɪʀᴇᴄᴛ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋꜱ ғᴏʀ ᴍᴏᴠɪᴇꜱ & ꜱᴇʀɪᴇꜱ 📥.")
         return
 
     # --- Case 2: Deep-link /start file_xxx ---
